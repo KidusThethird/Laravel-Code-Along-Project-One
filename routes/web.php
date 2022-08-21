4832953bff5cde32c4bc230b468b05af2b06181e
KidusThethird <kidus.z3rd@gmail.com>
2022-08-21 01:26:59 -0700

    Route_tests_made

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('hello/', function () {
+    return response('<h4>Hello world2</h4>' , 200)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo','bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Post '.$id);
+})->where('id', '[0-9]+');
