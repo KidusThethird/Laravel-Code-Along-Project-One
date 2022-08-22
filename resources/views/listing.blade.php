1d061d52266ecddee9615ba73ad09bffb0e4b082
KidusThethird <kidus.z3rd@gmail.com>
2022-08-22 06:12:37 -0700

    transition to layout

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,4 +1,6 @@
+@extends('layout')
 
+@section('content')
     <h2>
         {{$listing['title'];}}
     </h2>
@@ -7,3 +9,4 @@
     </p>
 
   
+    @endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,19 +1,27 @@
-<h1>{{$heading}}</h1>
+@extends('layout')
 
-@if(1>0)
-    @php echo "true"; @endphp
-@endif
+@section('content')
+
+<div  class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+  @unless(count($listings)==0)
 
 
 @foreach($listings as $listing)
     <h2>
      <a href="/laravel/laravel_project_one/public/listings/{{$listing['id']}}"> 
      
-     {{$listing['title'];}}
+     {{$listing['title'];}}</a> 
     </h2>
-</a>  
+ 
     <p>
         {{$listing['description'];}}
     </p>
 
     @endforeach
+
+   @else 
+   <p>No Listing Found!</p>
+   @endunless
+
+    @endsection
